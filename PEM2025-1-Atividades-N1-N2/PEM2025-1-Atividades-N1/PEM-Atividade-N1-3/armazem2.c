52ddc36f68bf4205e6ae15a33285bf431a7b6ccd
RUAN-VICTOR-10 <ruan.santos10@fatec.sp.gov.br>
2025-03-16 14:47:35 -0300

    refatorando variáveis e verificação de opções

--- PEM2025-1-Atividades-N1-N2/PEM2025-1-Atividades-N1/PEM-Atividade-N1-3/armazem2.c
+++ PEM2025-1-Atividades-N1-N2/PEM2025-1-Atividades-N1/PEM-Atividade-N1-3/armazem2.c
@@ -18,15 +18,15 @@
 #define MAX_PRODUTO 5    // Formato para o codigo do produto: Letra + 3 digitos (A123\0)
 
 // Funcao para validar o codigo do produto (Letra + 3 digitos)
-int validar_codigo_produto(char *codigo_produto) {
-    if (strlen(codigo_produto) != 4) { // se tamanho do codigo for diferente de 4. para.
+int validarCodigoProduto(char *codigoProduto) {
+    if (strlen(codigoProduto) != 4) { // se tamanho do codigo for diferente de 4. para.
         return 0;
     }
-    if (!isalpha(codigo_produto[0])) { // se o primeiro valor for diferente de uma letra. para.
+    if (!isalpha(codigoProduto[0])) { // se o primeiro valor for diferente de uma letra. para.
         return 0;
     }
     for (int i = 1; i < 4; i++) { // se no indice 1, 2 e 3 for diferente de numeros. para
-        if (!isdigit(codigo_produto[i])) {
+        if (!isdigit(codigoProduto[i])) {
             return 0;
         }
     }
@@ -34,20 +34,20 @@ int validar_codigo_produto(char *codigo_produto) {
 }
 
 // Funcao para estocar um produto dentro do limite de 5 lotes por gondola
-void estocar_produto(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO], char *codigo_produto) {
+void estocarProduto(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO], char *codigoProduto) {
     for (int rua = 0; rua < NUM_RUAS; rua++) {
         for (int gondola = 0; gondola < NUM_GONDOLAS; gondola++) {
             //strcmp eh usada para comparar strings
-            if (strcmp(armazem[rua][gondola][0], codigo_produto) == 0) { // se strings forem iguais...
+            if (strcmp(armazem[rua][gondola][0], codigoProduto) == 0) { // se strings forem iguais...
                 for (int i = 0; i < MAX_LOTES; i++) {
                     if (strlen(armazem[rua][gondola][i]) == 0) { // se a posicao da matriz estiver vazia
-                        strcpy(armazem[rua][gondola][i], codigo_produto); //copia o codigo do produto para a posicao da matriz
-                        printf("Lote do produto %s adicionado na Gondola %c%d.\n", codigo_produto, 'A' + rua, gondola + 1);
+                        strcpy(armazem[rua][gondola][i], codigoProduto); //copia o codigo do produto para a posicao da matriz
+                        printf("Lote do produto %s adicionado na Gondola %c%d.\n", codigoProduto, 'A' + rua, gondola + 1);
                         return;
                     }
                 }
                 //senao, a gondola esta cheia
-                printf("A Gondola %c%d esta cheia para o produto %s.\n", 'A' + rua, gondola + 1, codigo_produto);
+                printf("A Gondola %c%d esta cheia para o produto %s.\n", 'A' + rua, gondola + 1, codigoProduto);
                 return;
             }
         }
@@ -56,8 +56,8 @@ void estocar_produto(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO
     for (int rua = 0; rua < NUM_RUAS; rua++) {
         for (int gondola = 0; gondola < NUM_GONDOLAS; gondola++) {
             if (strlen(armazem[rua][gondola][0]) == 0) { // a gondola esta vazia?
-                strcpy(armazem[rua][gondola][0], codigo_produto); // copia o codigo do produto
-                printf("Produto %s estocado na Gondola %c%d.\n", codigo_produto, 'A' + rua, gondola + 1);
+                strcpy(armazem[rua][gondola][0], codigoProduto); // copia o codigo do produto
+                printf("Produto %s estocado na Gondola %c%d.\n", codigoProduto, 'A' + rua, gondola + 1);
                 return;
             }
         }
@@ -66,34 +66,34 @@ void estocar_produto(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO
 } 
 
 // Funcao para retirar um produto(lote) do estoque
-void retirar_produto(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO], char *codigo_produto) {
+void retirarProduto(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO], char *codigoProduto) {
     for (int rua = 0; rua < NUM_RUAS; rua++) {
         for (int gondola = 0; gondola < NUM_GONDOLAS; gondola++) {
             for (int i = 0; i < MAX_LOTES; i++) {
-                if (strcmp(armazem[rua][gondola][i], codigo_produto) == 0) { // se posicao do armazem for igual ao codigo...
+                if (strcmp(armazem[rua][gondola][i], codigoProduto) == 0) { // se posicao do armazem for igual ao codigo...
                     memset(armazem[rua][gondola][i], 0, sizeof(armazem[rua][gondola][i])); // todos os bytes na posicao especifica da matriz armazem sera substituido por 0
-                    printf("Lote do produto %s retirado da Gondola %c%d.\n", codigo_produto, 'A' + rua, gondola + 1);
+                    printf("Lote do produto %s retirado da Gondola %c%d.\n", codigoProduto, 'A' + rua, gondola + 1);
                     return;
                 }
             }
         }
     }
-    printf("Produto %s nao encontrado no estoque!\n", codigo_produto);
+    printf("Produto %s nao encontrado no estoque!\n", codigoProduto);
 }
 
 // Funcao para mostrar o status do estoque
-void mostrar_estoque(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO]) {
+void mostrarEstoque(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO]) {
     printf("\nStatus do estoque:\n");
     for (int i = 0; i < NUM_RUAS; i++) {
         printf("Rua %c:\n", 'A' + i);
         for (int j = 0; j < NUM_GONDOLAS; j++) {
-            int quantidade_lotes = 0;
+            int quantidadeLotes = 0;
             for (int k = 0; k < MAX_LOTES; k++) {
                 if (strlen(armazem[i][j][k]) > 0) {
-                    quantidade_lotes++;
+                    quantidadeLotes++;
                 }
             }
-            printf("  Gondola %d: %d lote(s) ocupado(s)\n", j + 1, quantidade_lotes);
+            printf("  Gondola %d: %d lote(s) ocupado(s)\n", j + 1, quantidadeLotes);
         }
     }
 }
@@ -101,7 +101,7 @@ void mostrar_estoque(char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO
 int main() {
     char armazem[NUM_RUAS][NUM_GONDOLAS][MAX_LOTES][MAX_PRODUTO] = {{{{0}}}}; // inicializa a estrutura do armazem
     int opcao; // armazena a escolha do usuario
-    char codigo_produto[MAX_PRODUTO]; // codigo do produto possui 5 campos
+    char codigoProduto[MAX_PRODUTO]; // codigo do produto possui 5 campos
 
     while (1) {
         printf("\nEscolha uma opcao:\n");
@@ -110,27 +110,31 @@ int main() {
         printf("3 - Mostrar o status do estoque\n");
         printf("4 - Sair\n");
         printf("Digite sua opcao: ");
-        scanf("%d", &opcao);
-
+          // Verificando se a entrada é um número válido
+        while (scanf("%d", &opcao) != 1 || opcao < 1 || opcao > 4) {
+        // Limpa o buffer de entrada
+        while(getchar() != '\n');  // Limpa o buffer
+        printf("Entrada invalida. Por favor, escolha uma das opçoes validas (1, 2, 3 ou 4): ");
+        }
         switch (opcao) { //bloco de decisao
             case 1: // se codigo estiver errado. exibir menssagem de erro. senao estocar produto
                 printf("Digite o codigo do produto (ex: S123): ");
-                scanf("%s", codigo_produto);
-                if (!validar_codigo_produto(codigo_produto)) { 
+                scanf("%s", codigoProduto);
+                if (!validarCodigoProduto(codigoProduto)) { 
                     printf("Codigo invalido! O codigo deve seguir o formato Letra + 3 digitos.\n");
                 } else {
-                    estocar_produto(armazem, codigo_produto);
+                    estocarProduto(armazem, codigoProduto);
                 }
                 break;
 
             case 2: // retirar produto do armazem
                 printf("Digite o codigo do produto: ");
-                scanf("%s", codigo_produto);
-                retirar_produto(armazem, codigo_produto);
+                scanf("%s", codigoProduto);
+                retirarProduto(armazem, codigoProduto);
                 break;
 
             case 3: // exibir status do estoque
-                mostrar_estoque(armazem);
+                mostrarEstoque(armazem);
                 break;
 
             case 4: // sair do programa
