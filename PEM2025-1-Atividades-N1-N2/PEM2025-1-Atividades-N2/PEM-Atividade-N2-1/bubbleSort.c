6dde3ed35463b2af35bc07f56cf6d2faa9a192c8
RUAN-VICTOR-10 <ruan.santos10@fatec.sp.gov.br>
2025-05-12 02:36:43 -0300

    adicionando comentarios aos programas

--- PEM2025-1-Atividades-N1-N2/PEM2025-1-Atividades-N2/PEM-Atividade-N2-1/bubbleSort.c
+++ PEM2025-1-Atividades-N1-N2/PEM2025-1-Atividades-N2/PEM-Atividade-N2-1/bubbleSort.c
@@ -1,47 +1,53 @@
 #include <stdio.h>
 
+// Função para preencher o vetor com valores informados pelo usuário
 void preencherVet(int *vet, int tam){
     printf("\nPREENCHA UM VETOR COM 10 POSICOES\n");
 
-    for (int i=0; i< tam; i++){
-        printf("DIGITE O %do VALOR: ", i+1);
-        scanf("%d", (vet + i));
+    for (int i = 0; i < tam; i++){
+        printf("DIGITE O %do VALOR: ", i + 1);
+        scanf("%d", (vet + i)); // Lê o valor e armazena na posição i do vetor
     }
 }
-// *vet e vet[] sao a mesma coisa para o compilador no parametro da funcao
+
+// Função que ordena o vetor com o algoritmo Bubble Sort
 int bubbleSort(int *vet, int tam) {
     int aux, i;
-    int troca;
-    int *ptr;
+    int troca;     // variavel que evita loops desnecessarios
+    int *ptr;      // Ponteiro para percorrer o vetor
 
     do {
         troca = 0;
         for (ptr = vet; ptr < vet + tam - 1; ptr++) {
+            // Compara o valor atual com o próximo
             if (*ptr > *(ptr + 1)) {
+                // Troca os valores usando variável auxiliar
                 aux = *(ptr + 1);
                 *(ptr + 1) = *ptr;
                 *ptr = aux;
-                troca = 1;
+                troca = 1; 
             }
         }
-     tam--;
-    } while (troca);
+        tam--; 
+    } while (troca); // Continua enquanto houver trocas
 }
 
+// Função que imprime os valores do vetor
 void imprimir(int *vet, int tam) {
     printf("\nORDENACAO POR BUBBLE SORT\n");
     for (int i = 0; i < tam; i++) {
-        printf("vetor[%d] = %d\n", i, *(vet+i));
+        printf("vetor[%d] = %d\n", i, *(vet + i)); 
     }
 }
 
+// Função principal
 int main() {
     int vetor[10];
     int tam = sizeof(vetor) / sizeof(vetor[0]);
-    
-    preencherVet(vetor, tam);
-    bubbleSort(vetor, tam); 
-    imprimir(vetor, tam);
-   
+
+    preencherVet(vetor, tam);    
+    bubbleSort(vetor, tam);      
+    imprimir(vetor, tam);       
+
     return 0;
 }
--- PEM2025-1-Atividades-N1-N2/PEM2025-1-Atividades-N2/PEM-Atividade-N2-1/insertionSort.c
+++ PEM2025-1-Atividades-N1-N2/PEM2025-1-Atividades-N2/PEM-Atividade-N2-1/insertionSort.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 
-
+// Função para preencher o vetor com valores fornecidos pelo usuário
 void preencherVet(int *vet, int tam){
     printf("\nPREENCHA UM VETOR COM 10 POSICOES\n");
 
-    for (int i=0; i< tam; i++){
-        printf("DIGITE O %do VALOR: ", i+1);
+    for (int i = 0; i < tam; i++) {
+        // Solicita ao usuário que digite um valor e armazena no vetor usando ponteiro
+        printf("DIGITE O %do VALOR: ", i + 1);
         scanf("%d", (vet + i));
     }
 }
-// Função que ordena um vetor utilizando o método insertion sort com ponteiros
+
+// Função que ordena o vetor usando o algoritmo Insertion Sort
 void insertionSort(int *vet, int tam) {
     for (int i = 1; i < tam; i++) {
-        int *ptr = vet + i;  // vet + i refere-se ao endereco
-        int aux = *ptr;
-        int *j = ptr - 1;
+        int *ptr = vet + i;  // Ponteiro para o endereço atual
+        int aux = *ptr;      // Armazena o valor atual
+        int *j = ptr - 1;    // Ponteiro para o endereço anterior
 
+        // Move elementos maiores que 'aux' uma posição à frente
         while (j >= vet && *j > aux) {
-            *(j + 1) = *j;
-            j--;
+            *(j + 1) = *j;   // Desloca o valor para frente
+            j--;             // Move o ponteiro uma posição para trás
         }
+
+        // Insere o valor de 'aux' na posição correta
         *(j + 1) = aux;
     }
 }
 
+// Função para imprimir o vetor ordenado
 void imprimir(int *vet, int tam) {
     printf("\nORDENACAO POR INSERTION SORT\n");
     for (int i = 0; i < tam; i++) {
@@ -33,11 +39,11 @@ void imprimir(int *vet, int tam) {
 
 int main() {
     int vetor[10]; 
-    int tam = sizeof(vetor) / sizeof(vetor[0]);
+    int tam = sizeof(vetor) / sizeof(vetor[0]); // Calcula o tamanho do vetor
 
-    preencherVet(vetor, tam);
-    insertionSort(vetor, tam);
-    imprimir(vetor, tam);
+    preencherVet(vetor, tam);  
+    insertionSort(vetor, tam);  
+    imprimir(vetor, tam);      
     
-    return 0;
+    return 0; 
 }
